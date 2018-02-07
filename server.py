24a861b9f27f86ed716e0e60ecfb8db374336981
agedob <agedob888@gmail.com>
2018-02-06 18:29:25 -0800

    almost done trying key/values

--- server.py
+++ server.py
@@ -1,12 +1,36 @@
-students = [
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+
+# def stus(mon):
+#     for i in mon:
+#         print i['first_name'], i['last_name']
+# stus(students)
+
+
+users = {
+ 'Students': [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
 
-def stus(mon):
-    for i in mon:
-        print i['first_name'], i['last_name']
-        # print i.values()
-stus(students)
+# def monkey(tuse):
+#     for key in tuse:
+#         # print tuse["Students"][3]['first_name']
+#         for guess in range(len(tuse[key])):
+#             a = tuse[key][guess]['first_name']
+#             b = tuse[key][guess]['last_name']
+#             print guess+1, '-', a, b, '-', len(a)+len(b)
+# monkey(users)
+
+def
